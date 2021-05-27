e0a2fbfdd60a75a9cecc53b02629c9748af70e0a
dianabarsanescu <65299933+barsanescudiana@users.noreply.github.com>
2021-05-27 13:22:45 +0300

    Test Case w/ setUp @ JUnit 4

--- Seminar 13/src/ro/ase/cts/tests/GrupaTests.java
+++ Seminar 13/src/ro/ase/cts/tests/GrupaTests.java
@@ -2,12 +2,16 @@ package ro.ase.cts.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
 
 public class GrupaTests {
 
+	
+	
 	@Test
 	public void testConstructorRight() {
 		Grupa grupa = new Grupa(1076);
@@ -50,4 +54,56 @@ public class GrupaTests {
 		assertNotNull(grupa.getStudenti());
 	}
 	
+	@Test
+	public void testPromovabilitateRight() {
+		Grupa grupa = new Grupa(1076);
+		for(int i = 0; i<=7; i++) {
+			Student student = new Student("Diana");
+			student.adaugaNota(4);
+			student.adaugaNota(7);
+			student.adaugaNota(3);
+			grupa.adaugaStudent(student);
+		}
+		for(int i = 0; i<=5; i++) {
+			Student student = new Student("Anca");
+			student.adaugaNota(9);
+			student.adaugaNota(8);
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(0.42, grupa.getPromovabilitate(), 0.01);
+	}
+	
+	@Test
+	public void testPromovabilitateBoundaryInf() {
+		Grupa grupa = new Grupa(1076);
+		for(int i = 0; i<=7; i++) {
+			Student student = new Student("Diana");
+			student.adaugaNota(4);
+			student.adaugaNota(7);
+			student.adaugaNota(3);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(0.0, grupa.getPromovabilitate(), 0.01);
+	}
+	
+	@Test
+	public void testPromovabilitateBoundarySup() {
+		Grupa grupa = new Grupa(1076);
+		for(int i = 0; i<=5; i++) {
+			Student student = new Student("Anca");
+			student.adaugaNota(9);
+			student.adaugaNota(8);
+			student.adaugaNota(10);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(1.0, grupa.getPromovabilitate(), 0.01);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testPromovabilitateError() {
+		Grupa grupa = new Grupa(1075);
+		grupa.getPromovabilitate();
+	}
+
 }
